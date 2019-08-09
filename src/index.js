7c49383c6bf7a68905c282b1053df3e1b4b8b9b6
Oksana <aveerudkevich@gmail.com>
2019-08-09 16:17:23 +0200

    added state in todo-list-item component

--- src/index.js
+++ src/index.js
@@ -1,58 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header/app-header';
-import SearchPanel from './components/search-panel/search-panel';
-import TodoList from './components/todo-list/todo-list';
-import './index.css';
-
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make React App', important: true, id: 2},
-        {label: 'Have a lunch', important: false, id: 3},
-    ];
-
-    return (
-        <div className='todo-app'>
-            <AppHeader toDo={1} done={3}/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    )
-};
-
-
-
+import App from './components/app/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
