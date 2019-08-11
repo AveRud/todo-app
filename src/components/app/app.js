a56aa265c784933271855417aa14fdd23ee0cd0d
Oksana <aveerudkevich@gmail.com>
2019-08-11 15:41:38 +0200

    implemented task deleting

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
 import TodoList from '../todo-list/todo-list';
 import ItemStatusFilter from '../item-status-filter/item-status-filter';
+import ItemAddForm from '../item-add-form/item-add-form'
+
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+    state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make React App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3},
+        ]
+    };
 
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make React App', important: true, id: 2},
-        {label: 'Have a lunch', important: false, id: 3},
-    ];
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const indexOfDeletedItem = todoData.findIndex((el) => el.id === id)
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3}/>
-            <div className="top-panel d-flex">
-                <SearchPanel/>
-                <ItemStatusFilter/>
+            const newArray = [
+                ...todoData.slice(0, indexOfDeletedItem),
+                ...todoData.slice(indexOfDeletedItem + 1)
+            ];
+
+            return {
+                todoData: newArray
+            };
+        });
+    };
+
+    addItem = (text) => {
+        console.log('Added ', text);
+    };
+
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={this.deleteItem}
+                />
+                <ItemAddForm onItemAdded={this.addItem}/>
             </div>
-
-            <TodoList
-                todos={todoData}
-                onDeleted={(id) => {
-                    console.log(`del`, id)
-                }}/>
-        </div>
-    )
-};
-
-export default App;
+        )
+    };
+}
