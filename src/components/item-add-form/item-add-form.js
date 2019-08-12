e68a98c732610e6e8afca18aa2dd0d464f99956c
Oksana <aveerudkevich@gmail.com>
2019-08-12 10:43:12 +0200

    little change: clean input after submitting

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -17,17 +17,21 @@ export default class ItemAddForm extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        })
 
     };
 
     render() {
         return (
             <form className='item-add-form d-flex'
-            onSubmit={this.onSubmit}>
+                  onSubmit={this.onSubmit}>
                 <input type="text"
                        className='form-control'
                        onChange={this.onLabelChange}
-                       placeholder='what need to be done'/>
+                       placeholder='what need to be done'
+                       value={this.state.label}/>
                 <button
                     className='btn btn-outline-secondary'>
                     Add Item
