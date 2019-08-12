da6a73322b5f1d3f44d80a867ea0e43b6dcf524b
Oksana <aveerudkevich@gmail.com>
2019-08-12 12:14:53 +0200

    implement search feature

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './search-panel.css'
 
-const SearchPanel = () => {
-    return (
-        <input type="text"
-               className='form-control search-input'
-               placeholder="search task..."/>
-    );
-};
+export default class SearchPanel extends Component {
 
-export default SearchPanel;
+    state = {
+        term: ''
+    };
+
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onSearchChange(term);
+    };
+
+    render() {
+        return (
+            <input type="text"
+                   className='form-control search-input'
+                   placeholder="search task..."
+                   value={this.state.term}
+                   onChange={this.onSearchChange}/>
+        );
+    };
+}
